662eeb92b982e334313357b682c4cf8b7a464521
duribesaul <duribesaul@gmail.com>
2022-05-25 16:52:38 -0500

    Ejercicios JAVASCRIPT

---
+++ JS/main.js
@@ -0,0 +1,71 @@
+// //1.Solicitar 3 numeros (entre el 1 y el 100) y definir cual es el mayor.
+// let a = prompt("Escribe un numero del 1 al cien");
+// let b = prompt("Escribe un segundo numero del 1 al cien");
+// let c = prompt("Escribe un tercer numero del 1 al cien");
+
+
+// if (a > b && c){
+//         alert(a + " es el mayor");
+// }else if (b > a && c){
+//         alert(b + " es el mayor"); 
+
+// }else if (c > a && b){
+//         alert(c + " es el mayor");
+// }
+
+
+
+// //2.Solicitar 3 numeros (entre el 1 y el 100) y definir el menor de los tres .
+
+// let d = prompt("Escribe un numero del 1 al cien");
+// let f = prompt("Escribe un segundo numero del 1 al cien");
+// let g = prompt("Escribe un tercer numero del 1 al cien");
+
+// if (d < f && g){
+//     alert(d + " es el menor");
+// }else if (f < d && g){
+//     alert(f + " es el menor"); 
+
+// }else if(g < d && f){
+//     alert(g + " es el menor");
+// }
+// //4.Solicitar un entero ( entre el 100 y el 200) y determinar si es multiplo de 3 )
+// let e = prompt("Introduce un entero del 100 al 200")
+// if (e % 3 == 0 ) {
+//     alert("Es multiplo de 3")
+// }else {
+//     alert("No es multiplo de 3")
+// }
+
+
+
+//5.Elaborar un algritmo para leer 3 numeros y determinar si uno es la suma de los otros
+let  h = prompt("Escribe un numero del 1 al cien");
+let  i = prompt("Escribe un segundo numero del 1 al cien");
+let  j = prompt("Escribe un tercer numero del 1 al cien");
+
+
+if ( h+i == j){
+    alert(j + " es la suma");
+}else if (i+j  == h){
+    alert(h + " es la suma"); 
+
+}else if(h+j  == i){
+    alert(i + " es la suma");
+}else{
+    alert("no es suma");
+}
+
+
+
+
+
+
+// //6.Elaborar un algoritmo para leer un numero y determinar si es par o impar
+
+// let z = prompt("Introduce un entero del 100 al 200")
+// if (z % 2 == 0 ) {
+//     alert("Es par")
+// }else {
+//     alert("es impar")
+// }
